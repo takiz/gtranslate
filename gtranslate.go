7a236aac0273137008c3033e66dc39472198aac0
takiz <koechto2@yandex.ru>
2020-05-04 17:05:40 +0500

    add replace

--- gtranslate.go
+++ gtranslate.go
@@ -3,7 +3,6 @@ package main
 import (
 	"fmt"
 	"io/ioutil"
-	"log"
 	"net/http"
 	"net/url"
 	"os"
@@ -15,39 +14,30 @@ func main() {
 		fmt.Fprintf(os.Stderr, "Usage: %s \"text\"\n", os.Args[0])
 		os.Exit(1)
 	}
-
+	echeck := func(err error) {
+		if err != nil {
+			fmt.Fprintln(os.Stderr, err)
+			os.Exit(1)
+		}
+	}
 	Url := "http://translate.googleapis.com/translate_a/single?client=gtx&sl=auto&tl=ru&dt=t&"
 	Ua := "Mozilla/5.0"
 
 	client := &http.Client{}
 	s := make(url.Values)
-	s.Add("q", os.Args[1])
+	s.Add("q", strings.TrimSpace(os.Args[1]))
 	Url = Url + s.Encode()
 	req, err := http.NewRequest("GET", Url, nil)
-	if err != nil {
-		log.Fatal(err)
-	}
+	echeck(err)
 	req.Header.Set("User-Agent", Ua)
 	resp, err := client.Do(req)
-	if err != nil {
-		log.Fatal(err)
-	}
+	echeck(err)
 	data, err := ioutil.ReadAll(resp.Body)
 	resp.Body.Close()
-	if err != nil {
-		log.Fatal(err)
-	}
+	echeck(err)
 
-	length := len(data)
-	data2 := make([]byte, length)
-	for j, i := 0, 0; i < length; i++ {
-		if data[i] != '\n' {
-			data2[j] = data[i]
-			j++
-		}
-	}
-	str := string(data2[4:])
-	length = len(str)
+	str := strings.ReplaceAll(string(data[4:]), "\n", "")
+	length := len(str)
 	for i := 0; i < length; i++ {
 		if n := strings.Index(str[i:], "\",\""); n != -1 {
 			if i == 0 {
@@ -56,8 +46,7 @@ func main() {
 			if m := strings.Index(str[i:i+n], "],["); m != -1 {
 				for _, t := range str[i+m+4 : i+n] {
 					if t >= 1000 {
-						fmt.Fprintf(os.Stdout, "%s",
-							str[i+m+4:i+n])
+						fmt.Fprintf(os.Stdout, "%s", str[i+m+4:i+n])
 						break
 					}
 				}
